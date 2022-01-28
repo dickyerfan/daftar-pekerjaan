24f00a90ac37863225bf58131f14dfdf23f4c0a4
dickyerfan <dickyerfan@gmail.com>
2022-01-28 09:01:12 +0700

    merubah tampilan pengaturan

--- profil.php
+++ profil.php
@@ -80,47 +80,54 @@ switch ($bulan_lahir) {
             <div class="col-sm-8">
                 <div class="card shadow-lg">
                     <div class="card-header bg-primary">
-                        <div class="row">
-                            <div class="col-sm-7">
-                                <h2 class="text-light">PROFIL KARYAWAN</h2>
+                        <div class="row d-flex">
+                            <div class="col-sm-9">
+                                <h2 class="text-light float-start">PROFIL KARYAWAN</h2>
                             </div>
-                            <div class="col-sm-1 text-end">
-                                <a href="update.php" class="btn btn-outline-light" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Update Jabatan" style="width: 75px;"><img src="img/vector-pen.svg" alt="" style="width: 2rem;"></a>
-                            </div>
-                            <div class="col-sm-2 text-end">
-                                <a href="gantipass.php" class="btn btn-outline-light" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ganti Password" style="width: 75px;"><img src="img/key.svg" alt="" style="width: 2rem;"></a>
-                            </div>
-                            <div class="col-sm-2 text-end">
-                                <a href="index.php" class="btn btn-outline-light" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Kembali ke Halaman Utama" style="width: 75px;"><img src="img/x-circle.svg" alt="" style="width: 2rem;"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-sm-8">
-                            <div class="card shadow mt-2 mb-0">
-                                <h2 class="card-title text-center mt-1"><?php echo $_SESSION['nama_depan'] . ' ' . $_SESSION['nama_belakang']; ?></h2>
-                                <h4 class="card-title text-center"><?php echo $_SESSION['jabatan']  . ' - ' . $_SESSION['sub_bagian']; ?></h4>
+                            <div class="col-sm-3">
+                                <ul class="nav nav-tabs card-header-tabs float-end">
+                                    <div class="dropdown">
+                                        <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Pengaturan
+                                        </button>
+                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                            <li><a class="dropdown-item" href="update.php"><img src="img/vector-pen.svg" alt="" style="width: 1.2rem;"> Update</a></li>
+                                            <li><a class="dropdown-item" href="gantipass.php"><img src="img/key.svg" alt="" style="width: 1.2rem;"> Ganti Password</a></li>
+                                            <li><a class="dropdown-item" href="index.php"><img src="img/backspace.svg" alt="" style="width: 1.2rem;"> Kembali</a></li>
+                                        </ul>
+                                    </div>
+                                </ul>
                             </div>
                         </div>
                     </div>
                     <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h5 class="card-title">Nik</h5>
-                                <h5 class="card-title">Tempat Lahir</h5>
-                                <h5 class="card-title">Tanggal Lahir</h5>
-                                <h5 class="card-title">Jenis Kelamin</h5>
-                                <h5 class="card-title">Agama</h5>
+                        <div class="row justify-content-center">
+                            <div class="col-sm-8">
+                                <div class="card shadow mt-2 mb-0">
+                                    <h2 class="card-title text-center mt-1"><?php echo $_SESSION['nama_depan'] . ' ' . $_SESSION['nama_belakang']; ?></h2>
+                                    <h4 class="card-title text-center"><?php echo $_SESSION['jabatan']  . ' - ' . $_SESSION['sub_bagian']; ?></h4>
+                                </div>
                             </div>
-                            <div class="col-sm-5">
-                                <h5 class="gantisize card-title"><?php echo $nik_baru; ?></h5>
-                                <h5 class="gantisize card-title"><?php echo ucwords(strtolower($_SESSION['tempat_lahir'])); ?></h5>
-                                <h5 class="card-title"><?php echo ucwords($tanggal_lahir . ' ' . $bulan_lahir . ' ' . $tahun_lahir); ?></h5>
-                                <h5 class="gantisize card-title"><?php echo ucwords(strtolower($_SESSION['jenkel'])); ?></h5>
-                                <h5 class="gantisize card-title"><?php echo ucwords(strtolower($_SESSION['agama'])); ?></h5>
-                            </div>
-                            <div class="col-sm-4 text-center">
-                                <img src="img/pdam_biru.png" alt="" style="width: 7rem;">
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h5 class="card-title">Nik</h5>
+                                    <h5 class="card-title">Tempat Lahir</h5>
+                                    <h5 class="card-title">Tanggal Lahir</h5>
+                                    <h5 class="card-title">Jenis Kelamin</h5>
+                                    <h5 class="card-title">Agama</h5>
+                                </div>
+                                <div class="col-sm-5">
+                                    <h5 class="gantisize card-title"><?php echo $nik_baru; ?></h5>
+                                    <h5 class="gantisize card-title"><?php echo ucwords(strtolower($_SESSION['tempat_lahir'])); ?></h5>
+                                    <h5 class="card-title"><?php echo ucwords($tanggal_lahir . ' ' . $bulan_lahir . ' ' . $tahun_lahir); ?></h5>
+                                    <h5 class="gantisize card-title"><?php echo ucwords(strtolower($_SESSION['jenkel'])); ?></h5>
+                                    <h5 class="gantisize card-title"><?php echo ucwords(strtolower($_SESSION['agama'])); ?></h5>
+                                </div>
+                                <div class="col-sm-4 text-center">
+                                    <img src="img/pdam_biru.png" alt="" style="width: 7rem;">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -128,9 +135,6 @@ switch ($bulan_lahir) {
             </div>
         </div>
     </div>
-
-
-
 
     <script src="js/bootstrap.bundle.js" crossorigin="anonymous"></script>
 
